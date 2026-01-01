a349ba5187840faa4d63438442b13e9201ece05e
hhhhoper <qingyiliu01@gmail.com>
2026-01-01 11:59:24 +0800

    feat: jwt

--- admin/src/main/java/com/lqy/admin/controller/AuthController.java
+++ admin/src/main/java/com/lqy/admin/controller/AuthController.java
@@ -1,6 +1,55 @@
 package com.lqy.admin.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lqy.admin.common.exception.BusinessException;
+import com.lqy.admin.common.result.Result;
+import com.lqy.admin.entity.SysUser;
+import com.lqy.admin.service.SysUserService;
+import com.lqy.admin.until.JwtUtil;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
+
+    @Autowired
+    private SysUserService systUserService;
+
+    @PostMapping("/login")
+    public Result<?> login(@RequestBody Map<String,String> loginData) {
+        //TODO: process POST request
+        String username=loginData.get("name");
+        String password=loginData.get("password");
+        
+        SysUser user=systUserService.getByUsername(username);
+        if(user==null){
+
+            throw new BusinessException(400,"用户不存在");
+
+        }
+        
+        //验证密码简单明文后面加密
+        if(!user.getPassword().equals(password)){
+            throw new BusinessException(400,"密码错误");
+        }
+        String token=JwtUtil.generateToken(username);
+        Map<String,String> data=new HashMap<>();
+        data.put("token", token);
+        return Result.success(data);
+    
+    
+    }
+    
+
 
     
 }
--- admin/src/main/java/com/lqy/admin/mapper/SysUserMapper.java
+++ admin/src/main/java/com/lqy/admin/mapper/SysUserMapper.java
@@ -1,12 +1,14 @@
 package com.lqy.admin.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import com.lqy.admin.entity.SysUser;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 @Mapper
 public interface SysUserMapper extends BaseMapper<SysUser> 
 {    
+    @Select("SELECT * FROM sys_user WHERE username = #{username}")
     SysUser selectByUsername(String username);
 
 }
